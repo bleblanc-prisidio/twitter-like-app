b77d52fa22866306eefb06f2b82ffb15d5f2e694
bleblanc-prisidio <brennan.leblanc@prisid.io>
2024-08-01 12:41:04 -0700

    Update
    - Updating routes

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use("/api", router);
+
 app.post('/api/data', (req, res) => {
     const json = req.body;
     res.status(200).json({message: 'JSON data received', data: json});
 });
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({error: "Internal Server Error"});
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`);
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,72 +1,18 @@
-const express = require("express");
-
+// Import express
+const express = require('express');
+// Import fs and promises
 const fs = require("fs/promises");
-
+// Import validatePostData
 const validatePostData = require("../middlewares/validateData");
 
 const router = express.Router();
 
+// Create a function to read the data from the database
 async function readData() {
     try {
-        const data = await fs.readFile("./database/posts.json");
-        return JSON.parse(data);
+    const data = await readFile("./database/post.json");
+    return JSON.parse(data);
     } catch (error) {
         throw error;
     }
 }
-
-router.get("/", async (req, res, next) => {
-    try {
-        const data = await readData();
-
-        res.status(200).send(data);
-    } catch (error) {
-        console.error(error.message);
-    }
-});
-
-router.get("/post/:id", async (req, res, next) => {
-    try {
-        const postId = req.params.id;
-
-        const data = await readData();
-
-        const post = data.find((post) => post.id == postId);
-
-        if (!post) {
-            res.status(404).json({error: "Post not found"});
-        } else {
-            res.status(200).send(post);
-        }
-    } catch (error) {
-        console.error(error.message);
-    }
-});
-
-// CREATE POST
-router.post("/", validatePostData, async (req, res, next) => {
-    try {
-      // Generate a unique ID for the new post
-      const newPost = {
-        id: Date.now().toString(),
-        username: req.body.username,
-        postTitle: req.body.postTitle,
-        postContent: req.body.postContent,
-      };
-  
-      // Read the existing data
-      const data = await readData();
-  
-      // Add the new post to the data
-      data.push(newPost);
-  
-      // Write the updated data back to the JSON file
-      await fs.writeFile("./database/posts.json", JSON.stringify(data));
-  
-      // Send a success response with the new post
-      res.status(201).json(newPost);
-    } catch (error) {
-      // Handle errors by logging them to the console
-      console.error(error.message);
-    }
-  });
