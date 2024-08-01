0e3631b899d88aea877cc91f00bb7691f672c180
bleblanc-prisidio <brennan.leblanc@prisid.io>
2024-08-01 11:08:43 -0700

    Update
    Testing

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-// Ordering the express library for us to implement and use. This is just importing it
 const express = require('express');
-
-// How do we use it?
-
-// First we need to assign it to a variable
+const cors = require('cors'); // Import the cors package
 
 const app = express();
 
-// How do we set up the server/port to listen on??
-
-// Lets define a port to use
-
 const PORT = process.env.PORT || 5000;
 
-// Lets tell our app to run on this port
+// Use the cors middleware
+app.use(cors());
 
-app.listen(PORT, (error) => {
-   if (!error) {
-    console.log(`App is listening on ${PORT}`);
-   } else {
-    console.log(`Error, Failed to listen on port ${PORT}`);
-}
+app.use(express.json());
+
+app.post('/api/data', (req, res) => {
+    const json = req.body;
+    res.status(200).json({message: 'JSON data received', data: json});
 });
 
-app.get('/', (req, res) => {
-    res.status(200);
-    res.send("welcome to the root");
-})
-
-app.get('/hello', (req, res) => {
-    res.set('Content-Type, text/html');
-    res.status(200).send("<h1> Hell GFG Learner! </h1>");
+app.listen(PORT, () => {
+    console.log(`App is listening on port: ${PORT}`);
 });
