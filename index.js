f8b5cfa03264cf4f35420327f613cc93759f607e
bleblanc-prisidio <brennan.leblanc@prisid.io>
2024-08-01 09:52:38 -0700

    update
    update

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+// Ordering the express library for us to implement and use. This is just importing it
 const express = require('express');
+
+// How do we use it?
+
+// First we need to assign it to a variable
 
 const app = express();
 
+// How do we set up the server/port to listen on??
+
+// Lets define a port to use
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log('App is listening on port 5000');
+// Lets tell our app to run on this port
+
+app.listen(PORT, (error) => {
+   if (!error) {
+    console.log(`App is listening on ${PORT}`);
+   } else {
+    console.log(`Error, Failed to listen on port ${PORT}`);
+}
+});
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send("welcome to the root");
 })
+
+app.get('/hello', (req, res) => {
+    res.set('Content-Type, text/html');
+    res.status(200).send("<h1> Hell GFG Learner! </h1>");
+});
