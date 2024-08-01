9a5fc172e89c8f44a8dd0d4d00b99c4bd4c82484
bleblanc-prisidio <brennan.leblanc@prisid.io>
2024-08-01 09:06:21 -0700

    Update
    Update

--- index.js
+++ index.js
@@ -2,19 +2,8 @@ const express = require('express');
 
 const app = express();
 
-const router = require("../routes/postsRoutes");
-
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-
-app.use("/api", router);
-
-BiAperture.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({error: "Internal Server Error"});
-});
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log('App is listening on port 5000');
+})
